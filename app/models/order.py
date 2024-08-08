5f723183ef337814425b012b941746508710ab5f
HillaArts <hassenyehillary@gmail.com>
2024-08-08 15:03:16 +0300

    order model

---
+++ app/models/order.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from app.extensions import db
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    total_price = db.Column(db.Float, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    status = db.Column(db.String(20), default='Pending')
+    user = db.relationship('User', backref=db.backref('orders', lazy=True))
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'total_price': self.total_price,
+            'created_at': self.created_at.isoformat(),
+            'status': self.status,
+        }
