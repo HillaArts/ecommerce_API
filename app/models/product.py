86508b4a04397240a7404584d33f147ad59440d4
HillaArts <hassenyehillary@gmail.com>
2024-08-08 15:00:54 +0300

    product model

---
+++ app/models/product.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from app.extensions import db
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False, index=True)
+    description = db.Column(db.String(500))
+    price = db.Column(db.Float, nullable=False)
+    stock = db.Column(db.Integer, default=0)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'price': self.price,
+            'stock': self.stock,
+            'created_at': self.created_at.isoformat(),
+        }
