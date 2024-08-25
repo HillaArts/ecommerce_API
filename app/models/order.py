d1c609c9c56884431b91655afdbc77504f080cfc
HillaArts <hassenyehillary@gmail.com>
2024-08-25 10:16:38 +0300

    Remove total_price Column from Database

--- app/models/order.py
+++ app/models/order.py
@@ -1,16 +1,26 @@
 from datetime import datetime
+from sqlalchemy.orm import column_property
+from sqlalchemy import select, func
 from app.extensions import db
+from app.models.orderproduct import OrderProduct
 
 class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    total_price = db.Column(db.Float, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     status = db.Column(db.String(20), default='Pending')
     
     # Relationships with descriptive backref names
     user = db.relationship('User', backref=db.backref('user_orders', lazy=True))
     order_products = db.relationship('OrderProduct', backref='associated_order', lazy=True)
+
+    # Calculate total price dynamically
+    total_price = column_property(
+        select(func.sum(OrderProduct.quantity * OrderProduct.price_at_order))
+        .where(OrderProduct.order_id == id)
+        .correlate_except(OrderProduct)
+        .scalar_subquery()
+    )
 
     def to_dict(self):
         return {
