b46937e74eb027705f98ce1f3da391588d66ade2
HillaArts <hassenyehillary@gmail.com>
2024-08-26 23:42:17 +0300

    update docs code to PEP 8 and PEP 257 standards

--- app/errors/__init__.py
+++ app/errors/__init__.py
@@ -2,6 +2,12 @@ from flask import Flask
 from .handlers import bad_request_error, not_found_error, internal_error
 
 def register_error_handlers(app: Flask):
+    """
+    Register error handlers for the Flask application.
+    
+    Args:
+        app (Flask): The Flask application instance.
+    """
     app.register_error_handler(400, bad_request_error)
     app.register_error_handler(404, not_found_error)
     app.register_error_handler(500, internal_error)
