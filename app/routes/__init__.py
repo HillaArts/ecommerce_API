3908de34e3899c9665cfaa6e563362d5f7250683
HillaArts <hassenyehillary@gmail.com>
2024-08-27 00:04:52 +0300

    update init routes to standard

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -4,7 +4,12 @@ from .product import bp as product_bp
 from .order import bp as order_bp
 
 def register_blueprints(app):
+    """
+    Register all blueprints with the Flask application.
+    
+    Args:
+        app (Flask): The Flask application instance.
+    """
     app.register_blueprint(auth_bp)
     app.register_blueprint(product_bp)
     app.register_blueprint(order_bp)
-    
