ccfe5e3d5411c5c913f46ad05ddf26e99a9dc45c
HillaArts <hassenyehillary@gmail.com>
2024-08-27 00:21:55 +0300

    update docs to standard and deleted unnecessry docs

--- app/docs.py
+++ app/docs.py
@@ -1,4 +1,10 @@
 from flasgger import Swagger
 
 def setup_swagger(app):
+    """
+    Set up Swagger for the Flask application.
+    
+    Args:
+        app (Flask): The Flask application instance.
+    """
     Swagger(app, template_file='static/swagger.yaml')
