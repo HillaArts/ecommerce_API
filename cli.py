52cc47bda162dad8a4b65147eb004bfc9bc9dddf
HillaArts <hassenyehillary@gmail.com>
2024-08-27 00:27:30 +0300

    update command-line interface to standard

--- cli.py
+++ cli.py
@@ -1,3 +1,7 @@
+"""
+This module provides a command-line interface for managing the Flask application.
+"""
+
 from flask.cli import FlaskGroup
 from app import create_app, db
 
@@ -6,6 +10,9 @@ cli = FlaskGroup(create_app=create_app)
 
 @cli.command("create_db")
 def create_db():
+    """
+    Create the database tables.
+    """
     db.create_all()
     print("Database created!")
 
